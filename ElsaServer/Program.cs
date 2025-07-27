b9b29d106455fe5088cfbef4c4215f0226d527f8
Massoud Asgariyan <38140244+oratiss@users.noreply.github.com>
2025-07-27 15:24:43 +0330

    retruning code to its original state

--- ElsaServer/Program.cs
+++ ElsaServer/Program.cs
@@ -2,10 +2,7 @@
 using Elsa.EntityFrameworkCore.Modules.Management;
 using Elsa.EntityFrameworkCore.Modules.Runtime;
 using Elsa.Extensions;
-using Elsa.Liquid.Services;
-using Fluid;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseStaticWebAssets();
@@ -24,13 +21,11 @@ services
         .UseWorkflowManagement(management =>
         {
             management.UseEntityFrameworkCore(ef => ef.UseSqlite());
-            //added by masoud asgarian
+            
+            //added by asgarian:
             management.SetDefaultLogPersistenceMode(Elsa.Workflows.LogPersistence.LogPersistenceMode.Include);
         })
-        .UseWorkflowRuntime(runtime =>
-        {
-            runtime.UseEntityFrameworkCore(ef => ef.UseSqlite());
-        })
+        .UseWorkflowRuntime(runtime => runtime.UseEntityFrameworkCore(ef => ef.UseSqlite()))
         .UseScheduling()
         .UseJavaScript()
         .UseLiquid()
@@ -41,36 +36,8 @@ services
         .AddWorkflowsFrom<Program>()
     );
 
-services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll", policy =>
-    {
-        policy.AllowAnyOrigin()
-        .AllowAnyMethod()
-        .AllowAnyHeader();
-    });
-});
+services.AddCors(cors => cors.AddDefaultPolicy(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().WithExposedHeaders("*")));
 services.AddRazorPages(options => options.Conventions.ConfigureFilter(new IgnoreAntiforgeryTokenAttribute()));
-
-services.AddEndpointsApiExplorer();
-services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("V1", new Microsoft.OpenApi.Models.OpenApiInfo
-    {
-        Title = "Elsa Workflows API",
-        Version = "V1",
-        Description = "API for Elsa Workflow Engine"
-    });
-
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description = "JWT Authorization header using the Bearer scheme",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer"
-    });
-});
 
 var app = builder.Build();
 
@@ -78,29 +45,16 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     app.UseHsts();
-
-
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Elsa Workflows API v1");
-        c.RoutePrefix = "swagger"; // This makes Swagger available at /swagger
-        c.DisplayRequestDuration();
-        c.EnableDeepLinking();
-        c.EnableFilter();
-        c.ShowExtensions();
-    });
 }
 
 app.UseHttpsRedirection();
 app.UseBlazorFrameworkFiles();
 app.UseRouting();
-app.UseCors("AllowAll");
+app.UseCors();
 app.UseStaticFiles();
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseWorkflowsApi();
 app.UseWorkflows();
 app.MapFallbackToPage("/_Host");
-
 app.Run();
