33789c422eb793696745978d1f8ebc5a33cb8af4
Massoud Asgariyan <38140244+oratiss@users.noreply.github.com>
2025-07-27 14:50:25 +0330

    initial commit

---
+++ ElsaServer/Program.cs
@@ -0,0 +1,106 @@
+﻿using Elsa.EntityFrameworkCore.Extensions;
+using Elsa.EntityFrameworkCore.Modules.Management;
+using Elsa.EntityFrameworkCore.Modules.Runtime;
+using Elsa.Extensions;
+using Elsa.Liquid.Services;
+using Fluid;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseStaticWebAssets();
+
+var services = builder.Services;
+var configuration = builder.Configuration;
+
+services
+    .AddElsa(elsa => elsa
+        .UseIdentity(identity =>
+        {
+            identity.TokenOptions = options => options.SigningKey = "large-signing-key-for-signing-JWT-tokens";
+            identity.UseAdminUserProvider();
+        })
+        .UseDefaultAuthentication()
+        .UseWorkflowManagement(management =>
+        {
+            management.UseEntityFrameworkCore(ef => ef.UseSqlite());
+            //added by masoud asgarian
+            management.SetDefaultLogPersistenceMode(Elsa.Workflows.LogPersistence.LogPersistenceMode.Include);
+        })
+        .UseWorkflowRuntime(runtime =>
+        {
+            runtime.UseEntityFrameworkCore(ef => ef.UseSqlite());
+        })
+        .UseScheduling()
+        .UseJavaScript()
+        .UseLiquid()
+        .UseCSharp()
+        .UseHttp(http => http.ConfigureHttpOptions = options => configuration.GetSection("Http").Bind(options))
+        .UseWorkflowsApi()
+        .AddActivitiesFrom<Program>()
+        .AddWorkflowsFrom<Program>()
+    );
+
+services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
+services.AddRazorPages(options => options.Conventions.ConfigureFilter(new IgnoreAntiforgeryTokenAttribute()));
+
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("V1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Elsa Workflows API",
+        Version = "V1",
+        Description = "API for Elsa Workflow Engine"
+    });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+});
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+
+
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Elsa Workflows API v1");
+        c.RoutePrefix = "swagger"; // This makes Swagger available at /swagger
+        c.DisplayRequestDuration();
+        c.EnableDeepLinking();
+        c.EnableFilter();
+        c.ShowExtensions();
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseBlazorFrameworkFiles();
+app.UseRouting();
+app.UseCors("AllowAll");
+app.UseStaticFiles();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseWorkflowsApi();
+app.UseWorkflows();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
